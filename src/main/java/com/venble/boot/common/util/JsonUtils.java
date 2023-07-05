779c18b1939531c2404520786918fd978cf25992
xbmlz <997909544@qq.com>
2023-07-05 15:23:00 +0800

    feat: 优化sql

--- src/main/java/com/venble/boot/common/util/JsonUtils.java
+++ src/main/java/com/venble/boot/common/util/JsonUtils.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-import java.util.Collection;
 import java.util.List;
 
 /**
